7fa61ede8288a1c774d7c474b39efffe61d91f36
MaksymTykhoniuk <mxtkhnk@gmail.com>
2023-03-15 18:15:16 +0200

    Routes

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,14 @@
+// import { useSearchParams } from 'react-router-dom';
+
+import { SearchBox } from 'components/SearchBox/SearchBox';
+
+const Movies = () => {
+  return (
+    <main>
+      <h1>Movies</h1>
+      <SearchBox />
+    </main>
+  );
+};
+
+export default Movies;
